dd47b6f9b70ea0d09f7e64492487387302033b4f
anilnamdev1989 <anilnamdev10@gmail.com>
2021-11-09 18:37:00 +0530

    good evening

---
+++ DemoMVC/Controllers/StudentRegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class StudentRegistrationController : Controller
+    {
+        // GET: StudentRegistration
+       
+        [HttpGet]
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(string fname,string lname)
+        {
+            return View();
+        }
+    }
+}
